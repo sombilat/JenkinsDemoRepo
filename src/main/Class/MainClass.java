cf44eb4fc8e6ac1d12cedcd6c2263d6532e8a937
Alexander T. SOMBILLO <sombilat@ph.ibm.com>
2018-05-07 22:31:33 +0800

    Removing invaliditiy of Head and such

--- src/main/Class/MainClass.java
+++ src/main/Class/MainClass.java
@@ -3,11 +3,7 @@ package main.Class;
 public class MainClass {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello World you are in the PROD Branch");
-=======
-		System.out.println("Hello Worlds");
->>>>>>> master
 	}
 
 }
