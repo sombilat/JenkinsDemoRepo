fb2bc6832c9ad625068ee185fd5e948a7ecada43
Alexander T. SOMBILLO <sombilat@ph.ibm.com>
2018-05-08 00:51:56 +0800

    Corrected Inconsistencies

--- src/main/Class/MainClass.java
+++ src/main/Class/MainClass.java
@@ -3,11 +3,7 @@ package main.Class;
 public class MainClass {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello Worlds");
-=======
 		System.out.println("Hello World you are in the PROD Branch");
->>>>>>> prod
 	}
 
 }
